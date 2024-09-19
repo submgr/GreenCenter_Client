6cf58bd08f41a833eb33c29a2a377862e6c188d7
Aram Virabyan <googlaram200@gmail.com>
2024-09-19 10:08:34 +0300

    upd

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,42 +1,49 @@
 <template>
     <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Home Page</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      <ion-content>
-        <ion-card>
-          <ion-card-header>
-            <ion-card-title>Добро пожаловать</ion-card-title>
-          </ion-card-header>
-          <ion-card-content>
-            В работе
-          </ion-card-content>
-        </ion-card>
-      </ion-content>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Home Page</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-card>
+                <ion-card-header>
+                    <ion-card-title>Добро пожаловать</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                    В работе
+                </ion-card-content>
+            </ion-card>
+        </ion-content>
     </ion-page>
-  </template>
-  
-  <script>
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/vue';
-  
-  export default {
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/vue';
+
+export default {
     name: 'HomePage',
     components: {
-      IonPage,
-      IonHeader,
-      IonToolbar,
-      IonTitle,
-      IonContent,
-      IonCard,
-      IonCardHeader,
-      IonCardTitle,
-      IonCardContent
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+        IonCard,
+        IonCardHeader,
+        IonCardTitle,
+        IonCardContent
+    },
+    mounted() {
+        const tabsEl = document.querySelector('ion-tab-bar');
+        if (tabsEl) {
+            tabsEl.hidden = true;
+            tabsEl.style.height = "1";
+        }
     }
-  };
-  </script>
-  
-  <style scoped>
-  /* Add your styles here */
-  </style>
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
