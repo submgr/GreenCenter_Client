c3f2a8b27612282a18633287aeb8ee6188632cae
Aram Virabyan <googlaram200@gmail.com>
2024-09-19 13:33:04 +0300

    uod

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,14 +2,17 @@
     <ion-page>
         <ion-content>
             <h1 style="margin-left: 22px; margin-top: 2.4rem; font-weight: 700; font-size: 34px;">Главная</h1>
-            <div @click="goto('/tabs/PointsMap')" style="width: calc(100% - 50px); margin-left: 20px; margin-right: 15px; height: auto; border-radius: 20px; margin-top: 25px;">
-                <img src="https://i.ibb.co/1qhJ7f5/1360-3x-3.png" style="border-radius: 20px;" />
+            <div @click="goto('/tabs/PointsMap')"
+                style="width: calc(100% - 50px); margin-left: 20px; margin-right: 15px; height: auto; border-radius: 20px; margin-top: 25px;">
+                <img :src="images[0]" style="border-radius: 20px;" />
             </div>
-            <div @click="goto('/tabs/tab1')" style="width: calc(100% - 50px); margin-left: 20px; margin-right: 15px; height: auto; border-radius: 20px; margin-top: 17px;">
-                <img src="https://i.ibb.co/C9YBBJT/1360-3x-6.png" style="border-radius: 20px;" />
+            <div @click="goto('/tabs/tab1')"
+                style="width: calc(100% - 50px); margin-left: 20px; margin-right: 15px; height: auto; border-radius: 20px; margin-top: 17px;">
+                <img :src="images[1]" style="border-radius: 20px;" />
             </div>
-            <div @click="goto('/tabs/Shorts/Upload')" style="width: calc(100% - 50px); margin-left: 20px; margin-right: 15px; height: auto; border-radius: 20px; margin-top: 25px;">
-                <img src="https://i.ibb.co/pWX05DY/Frame-1000000808-1.png" style="border-radius: 20px;" />
+            <div @click="goto('/tabs/Shorts/Upload')"
+                style="width: calc(100% - 50px); margin-left: 20px; margin-right: 15px; height: auto; border-radius: 20px; margin-top: 25px;">
+                <img :src="images[2]" style="border-radius: 20px;" />
             </div>
         </ion-content>
     </ion-page>
@@ -44,6 +47,15 @@ export default {
     methods: {
         goto(path) {
             this.$router.push(path);
+        }
+    },
+    data() {
+        return {
+            images: [
+                "https://i.ibb.co/1qhJ7f5/1360-3x-3.png",
+                "https://i.ibb.co/C9YBBJT/1360-3x-6.png",
+                "https://i.ibb.co/YyX0kyJ/Frame-1000000808-2.png"
+            ]
         }
     }
 };
